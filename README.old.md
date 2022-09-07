fab50b2022ecac28c890b170002333114d42b4eb
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-09-07 20:26:40 -0300

    feat:  body

---
+++ README.old.md
@@ -0,0 +1,5 @@
+# M4-tarefas-do-aranha
+npx create-react-app .
+{ao inves da gente colocar um "."no código acima a gente pode escrever o nome do nosso projeto, mas vai criar uma pasta com esse nome. O ponto é mais prático.}
+
+Vamos escluir todos os arquivos dentro de SRC e fazer os nossos
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";// biblioteca react. Apartir da versao 16, esta linha n e obri...// biblioteca dom que renderiza os componentes em tela
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.StrictMode>hola mundo</React.StrictMode>,
+  document.getElementById("root")
+);
