23e2a63c5017779251e510074ef1f850ce053457
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-09-08 16:19:16 -0300

    feat: style no css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,15 @@
 
 // se o export que estiver na função ei temos que import dentro de chaves import {capslok} from "./capslok.js"
 //import {capslok} from "./capslok.js"
-
+import "./style.css"
 //sempre que agebte for renderizar um componente ele tem que ter só um elemento
 export function Home (){
    return ( 
-    <> 
+    <div className="container"> 
     <h1> Tarefas do Aranha </h1>
     <input type="text" placeholder="Qual a tarefa do dia?"></input>
-   
-    </>
+    <button type="button">Adicionar</button>
+    </div>
    )
 }
 
